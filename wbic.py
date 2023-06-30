9183cda9138072e4b6dcc0561c040978d267e1de
KyleM73 <kyle.morgenstein@gmail.com>
2023-06-30 19:21:48 -0400

    working mpc

--- wbic.py
+++ wbic.py
@@ -70,7 +70,7 @@ class Env:
         links = self.client.getLinkStates(self.robot, [self.hip_name2idx[hip_name] for hip_name in self.hips])
         hip_poses = np.array([l[0] for l in links])
         footstep = self.controller.get_footstep_pose(x, hip_poses, t_stance)
-        print(footstep)
+        self.controller.act(x, hip_poses)
         assert False
         self.client.stepSimulation()
 
@@ -87,9 +87,11 @@ class Controller:
     def __init__(self, robot_urdf):
         self.robot = pin.buildModelFromUrdf(robot_urdf)
         self.data = self.robot.createData()
-
+        self.n = 4
+        self.ns = 13
         self.dt = 0.5
         self.k = 10
+        self.delta_t = self.dt/self.k
         self.m = 12
         self.mu = 0.6
         self.body_inertia = np.array([
@@ -98,8 +100,15 @@ class Controller:
             [0,0,0.45]
             ])
         self.indicator = np.array([[0],[0],[1]])
+        self.z_weight = 50
+        self.force_weight = 1e-6
+        self.fmin = 0
+        self.fmax = 200
+        self.qp_problem = None
+        self.Uz_last = 100 * np.ones((self.n*self.k))
+        self.c = np.array([[1/self.mu,0,0],[0,1/self.mu,0],[0,0,1]])
 
-    def act(self, x, hips, yaw_cmd=0, vx_cmd=1, vy_cmd=0, t_stance=0.5):
+    def act(self, x, hips, mpc_update=True, yaw_cmd=0, vx_cmd=1, vy_cmd=0):
         x = x.reshape(-1,1)
         ori = x[:3]
         pose = x[3:6]
@@ -108,40 +117,95 @@ class Controller:
         g = x[12]
         x_ref = self.get_ref_com(
             ori[2,0], pose[0,0], pose[1,0], pose[2,0],
-            yaw_cmd, vx_cmd, vy_cmd, self.dt, self.k
+            yaw_cmd, vx_cmd, vy_cmd
             )
-        footstep_des = self.get_footstep_pose(x, hips, t_stance)
+        t_stance = self.get_stance_time()
+        r = self.get_footstep_pose(x, hips, t_stance) #desired footstep location
         rot = self.rot(ori[2,0]).T
         A = np.block([
-            [np.eye(3), np.zeros((3,3)), rot*self.dt/self.k, np.zeros((3,3)), np.zeros((3,1))],
-            [np.zeros((3,3)), np.eye(3), np.zeros((3,3)), np.eye(3)*self.dt/self.k, np.zeros((3,1))],
+            [np.eye(3), np.zeros((3,3)), rot*self.delta_t, np.zeros((3,3)), np.zeros((3,1))],
+            [np.zeros((3,3)), np.eye(3), np.zeros((3,3)), np.eye(3)*self.delta_t, np.zeros((3,1))],
             [np.zeros((3,3)), np.zeros((3,3)), np.eye(3), np.zeros((3,3)), np.zeros((3,1))],
-            [np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3)), np.eye(3), self.indicator*self.dt/self.k], #np.array([[0],[0],[1]])
+            [np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3)), np.eye(3), self.indicator*self.delta_t], #np.array([[0],[0],[1]])
             [np.zeros((1,3)), np.zeros((1,3)), np.zeros((1,3)), np.zeros((1,3)), np.ones(1)]
             ])
         gI_inv = np.linalg.inv(rot@self.body_inertia@rot.T)
         B = np.block([
             [np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3))],
             [np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3))],
-            []
+            [gI_inv@self.get_skew_sym_mat(r[i])*self.delta_t for i in range(self.n)],
+            [np.eye(3)*self.delta_t for _ in range(self.n)],
+            [np.zeros((1,3)) for _ in range(self.n)]
             ])
+        self.MPC(A, B, x, x_ref)
 
+    def get_stance_time(self):
+        return np.array([[0.5,0.5,0.5,0.5]]).T
 
-    def get_ref_com(self, yaw, x, y, z, yaw_rate_cmd, vx_cmd, vy_cmd, dt, k):
+    def get_ref_com(self, yaw, x, y, z, yaw_rate_cmd, vx_cmd, vy_cmd):
         A = np.block([
-            [np.eye(3), np.zeros((3,3)), np.array([[0,0,0],[0,0,0],[0,0,dt/k]]), np.zeros((3,3)), np.zeros((3,1))],
-            [np.zeros((3,3)), np.eye(3), np.zeros((3,3)), np.eye(3)*dt/k, np.zeros((3,1))],
+            [np.eye(3), np.zeros((3,3)), np.array([[0,0,0],[0,0,0],[0,0,self.delta_t]]), np.zeros((3,3)), np.zeros((3,1))],
+            [np.zeros((3,3)), np.eye(3), np.zeros((3,3)), np.eye(3)*self.delta_t, np.zeros((3,1))],
             [np.zeros((3,3)), np.zeros((3,3)), np.eye(3), np.zeros((3,3)), np.zeros((3,1))],
             [np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3)), np.eye(3), np.zeros((3,1))], #np.array([[0],[0],[1]])
             [np.zeros((1,3)), np.zeros((1,3)), np.zeros((1,3)), np.zeros((1,3)), np.ones(1)]
             ])
-        A_stack = np.vstack([np.linalg.matrix_power(A,i) for i in range(k+1)])
+        A_stack = np.vstack([np.linalg.matrix_power(A,i) for i in range(1, self.k+1)])
         x0 = np.array([[0,0,yaw,x,y,z,0,0,yaw_rate_cmd,vx_cmd,vy_cmd,0,-9.8]]).T
         return A_stack @ x0
 
-    def MPC(self):
-        pass
+    def pow(self, A, i):
+        if i < 0:
+            return np.zeros_like(A)
+        elif i == 0:
+            return np.eye(A.shape[-1])
+        return np.power(A, i)
 
+
+    def MPC(self, A, B, x0, x_ref):
+        Aqp = np.block([
+            [np.power(A, i)]
+            for i in range(1, self.k+1)
+            ])
+        Bqp = np.zeros((13*self.k, 3*self.n*self.k))
+        for row_order in range(1, self.k+1):
+            Bqp[13*(row_order-1):13*row_order,:3*self.n*row_order] = np.block([self.pow(A,i-1)@B for i in range(row_order,0,-1)])
+
+        L = np.eye(13*self.k)
+        for i in range(self.k):
+            L[5+13*i,5+13*i] = self.z_weight
+        K = self.force_weight * np.eye(3*self.n*self.k)
+
+        H = 2*(Bqp.T @ L @ Bqp + K)
+        H = sp.sparse.csc_matrix(H)
+        g = 2*Bqp.T @ L @ (Aqp @ x0 - x_ref)
+
+        c_l = [np.array([-self.Uz_last[i], -self.Uz_last[i], self.fmin]) for i in range(self.n*self.k)]
+        c_low = np.block(c_l).reshape(-1, 1)
+        
+        c_h = [np.array([self.Uz_last[i], self.Uz_last[i], self.fmax]) for i in range(self.n*self.k)]
+        c_high = np.block(c_h).reshape(-1, 1)
+
+        C = sp.linalg.block_diag(*[self.c for _ in range(self.n*self.k)])
+        C = sp.sparse.csc_matrix(C)
+
+        if self.qp_problem is None:
+            self.qp_problem = osqp.OSQP()
+            self.qp_problem.setup(H, g, C, c_low, c_high, warm_start=True, verbose=False)
+            #self.qp_problem.codegen("c_code", python_ext_name="osqp_c")
+            #import osqp_c
+        else:
+            #osqp_c.update_P(H[H != 0], None)
+            self.qp_problem.update(Px=H, Ax=C, q=g, l=c_low, u=c_high)
+
+        result = self.qp_problem.solve()
+
+        if result.info.status != "solved":
+            raise ValueError("OSQP did not solve the problem!")
+
+        U = result.x
+        self.Uz_last = U[2::3]
+        return U
 
     def get_skew_sym_mat(self, v):
         assert v.size == 3
